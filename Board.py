12083da5d42e7fed4f2aef7fab4c8bea0e7349a4
Riley Schneider <riley@ferryry.de>
2022-11-28 15:47:48 +0100

    Initial Commit

--- Board.py
+++ Board.py
@@ -1,7 +1,12 @@
+import numpy as np
+
 class Board:
 
-    def create_board(self):
-        pass
+    board = []
+
+    def create_board(self) -> np.ndarray:
+        self.board = np.zeros(6, 7)
+        return self.board
 
     def __init__(self):
         pass
--- MainGame.py
+++ MainGame.py
@@ -1,8 +1,9 @@
 from Player import Player
-
+from Board import Board
 
 class MainGame:
     player = [Player()] * 2
+    board = Board()
 
     def get_username(self, idx) -> str:
         return str(self.player[idx].get_name())
@@ -12,10 +13,12 @@ class MainGame:
         self.player[1].set_name(input("PLayer 2: Enter your Name: "))
         print("{} is playing against {}".format(self.player[0].get_name(), self.player[0].get_name()))
 
-        self.player[0].set_symbol(input(f"{self.player[0].get_symbol()!s}: set your symbol: "))
-        self.player[1].set_symbol(input(f"{self.player[1].get_symbol()!s}: set your symbol: "))
+        self.player[0].set_symbol(input(f"{self.player[0].get_name()}: set your symbol: "))
+        self.player[1].set_symbol(input(f"{self.player[1].get_name()}: set your symbol: "))
         print("{} is using {}".format(self.player[0].get_name(), self.player[0].get_symbol()))
         print("{} is using {}".format(self.player[1].get_name(), self.player[1].get_symbol()))
+
+        print(self.board.create_board())
 
     def __init__(self):
         pass

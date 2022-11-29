c3b2087b3d501ab71deb3fef09e7448804acf977
Riley Schneider <riley@ferryry.de>
2022-11-29 11:47:29 +0100

    WIP: Add horizontal validation

--- StatusValidator.py
+++ StatusValidator.py
@@ -4,34 +4,49 @@ from Board import Board
 
 class StatusValidator:
 
-    def validate_vertical(self) -> bool:
+    def validate_vertically(self) -> bool:
         pass
 
-    def validate_horizontal(self) -> bool:
-        pass
+    def validate_horizontally(self) -> bool:
+        for player in self.players:
+            for row in self.board.get_board():
+                matches = 0
+                for index, column in enumerate(row):
+                    if matches == 4:
+                        return True
+
+                    if index == 6:
+                        break
+
+                    if column == player.get_symbol():
+                        matches += 1
+                    else:
+                        matches = 0
+
+        return False
+
 
     def validate_diagonally(self) -> bool:
         pass
 
     def validate(self) -> bool:
-        if (self.player[0].coins or self.player[1].coins) == 0:
+        if (self.players[0].coins or self.player[1].coins) == 0:
             return True
 
-        if self.validate_horizontal() or self.validate_vertical() or self.validate_diagonally():
+        #if self.validate_horizontal() or self.validate_vertical() or self.validate_diagonally():
+        #    return True
+
+        if self.validate_horizontally():
             return True
 
         return False
 
-
-
-    def is_winning(self, board: Board, player: list[Player]) -> bool:
-        self.player = player
+    def is_winning(self, board: Board, players: list[Player]) -> bool:
+        self.players = players
         self.board = board
         return self.validate()
 
-
-
     def __init__(self):
         self.winner = None
-        self.player = None
+        self.players = None
         self.board = None

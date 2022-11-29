4fcad4ab4eab6d9aa19a26932937672bfc8a5f6e
Riley Schneider <riley@ferryry.de>
2022-11-29 08:17:31 +0100

    Initial Commit

--- Board.py
+++ Board.py
@@ -1,8 +1,15 @@
 from os import linesep
-
+from typing import Optional
 
 class Board:
     board = []
+
+    def return_last_empty_slot(self, offset: int) -> Optional[int]:
+        for idx, item in enumerate(self.board):
+            if not str(item[offset]) == "0":
+                return idx
+        else:
+            return Optional[None]
 
     def create(self) -> None:
         self.board = [[0 for col in range(7)] for row in range(6)]
